7b92c2d76588b627ba6e24335c6fcc959ac79569
yasin ziyai <yasinziyai@gmail.com>
2024-08-12 17:48:25 +0330

    ....

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -2,7 +2,7 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
 async function BuyResidential({ searchParams }) {
-  const res = await fetch("http://localhost:3000/api/profile", {
+  const res = await fetch(`${process.env.VERCEL_URL}/api/profile`, {
     cache: "no-store",
   });
   const data = await res.json();
